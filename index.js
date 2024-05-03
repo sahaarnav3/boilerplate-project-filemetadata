d611eb717f105b30c2224c528aaec52edc3d0c7a
Arnav Saha <sahaarnav3@gmail.com>
2024-05-03 12:48:54 +0530

    Done, khatam, over..

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ require('dotenv').config()
 
 var app = express();
 
+var multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
+
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+//the main router logic
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => { // .single = a single file, and inside bracket will be the name
+  // res.json(req.file);
+  res.json({ "name":  req.file.originalname, "type": req.file.mimetype, "size": req.file.size});
+})
 
 
 
